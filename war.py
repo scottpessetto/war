842b1e979cda1d8678906080c4fe75223112d818
Scoot <scott.pessetto@gmail.com>
2021-09-24 12:26:02 -0800

    update war class

--- war.py
+++ war.py
@@ -2,24 +2,36 @@ import pandas as pd
 import numpy as np
 import random
 
+
 class war_deck:
     def __init__(self, seed):
-       self.seed=seed
-    
+        self.seed = seed
+
     def create_deck(self):
-       self.deck = [1,2,3,4,5,6,7,8,9,10,11,12,13,14] * 4
+        self.deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
 
     def shuffle_deck(self):
-        #self.deck = random.shuffle(self.deck)
-        self.deck=(random.sample(self.deck,len(self.deck)))
+        # self.deck = random.shuffle(self.deck)
+        self.deck = random.sample(self.deck, len(self.deck))
 
     def split_deck(self):
-        self.p1 = self.deck[:len(self.deck)//2]
-        self.p2 = self.deck[len(self.deck)//2:]
+        self.p1 = self.deck[: len(self.deck) // 2]
+        self.p2 = self.deck[len(self.deck) // 2 :]
 
-    def simulate(self):
-        if (len(p1)>0) && (len(p2)>0):
-            p1_card = p1.pop()
-            p2_card = p2.pop()
-        elif:
-            print('Game over')
+    def draw(self):
+        if (len(self.p1) > 0) & (len(self.p2) > 0):
+            self.p1_card = self.p1.pop()
+            self.p2_card = self.p2.pop()
+            if self.p1_card > self.p2_card:
+                self.p1.insert(0, self.p2_card)
+                self.p1.insert(0, self.p1_card)
+
+            elif self.p1_card == self.p2_card:
+                print("tie")
+            else:
+                self.p2.insert(0, self.p1_card)
+                self.p2.insert(0, self.p1_card)
+        else:
+            print("Game over")
+            print(self.p1.len())
+            print(self.p2.len())
